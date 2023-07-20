962c510069c2fe799a7ecbcab41d90136cf28612
martinezjm1968 <121067795+martinezjm1968@users.noreply.github.com>
2023-07-20 12:31:55 -0300

    Entrega 4 re entrega V1

--- Entregable4/Tienda_Online/src/utils.js
+++ Entregable4/Tienda_Online/src/utils.js
@@ -1,6 +1,4 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+import {dirname} from "path"
+import { fileURLToPath } from "url"
 
-
-
-export const __dirname = path.dirname(fileURLToPath(import.meta.url));
+export  const __dirname=dirname(fileURLToPath(import.meta.url))
