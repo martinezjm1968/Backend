473a1242bc25493a16c11e8bba4c63f78dcae7c3
martinezjm1968 <martinezjm6666@gmail.com>
2023-10-16 11:38:05 -0300

    Entrega Logger

--- Entrega_autenticacion_github/src/app.js
+++ Entrega_autenticacion_github/src/app.js
@@ -21,9 +21,11 @@ import { productsRouter } from "./routes/products.routes.js";
 import { cartsRouter } from "./routes/carts.routes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { generateUser } from './utils/helpers.js'; 
+import { addLogger } from "./helpers/logger.js";
 
 const PORT = config.server.port;
 const app = express();
+const logger = addLogger();
 
 //middlewares
 app.use(express.json()); // json en el body de una petición
@@ -32,10 +34,12 @@ app.use(express.static(__dirname + "/public"));
 //servidor de express
 const httpServer = app.listen(PORT, () => {
     try {
-        console.log(`Listening to the port ${PORT}\n`);
+        logger.info("Server ok")
+        //console.log(`Listening to the port ${PORT}\n`);
     }
     catch (err) {
-        console.log(err);
+        logger.info("Server error", err)
+        //console.log(err);
     }
 });
 
@@ -99,5 +103,22 @@ app.get("/api/users", (req,res)=>{
     res.json({status:"success", data:users});
 });
 
+
+// Logger
+app.get("/operacionSencilla",(req,res)=>{
+    let sum=0;
+    for(let i=0;i<1000;i++){
+        sum+=i;
+    }
+    res.send(`La suma es igual a ${sum}`);
+});
+
+app.get("/operacionCompleja",(req,res)=>{
+    let sum=0;
+    for(let i=0;i<5e7;i++){//50.000.000
+        sum+=i;
+    }
+    res.send(`La suma es igual a ${sum}`);
+});
 
 
